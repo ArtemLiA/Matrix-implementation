da010b497b9fffd2497e4332e48dab5fa99b31ef
Artem Liakhov <Liakhov.Artem@yandex.ru>
2022-12-01 16:13:42 +0300

    First commit: added a prototype of the array class

--- array.h
+++ array.h
@@ -4,10 +4,22 @@
 
 #ifndef MATRIX_ARRAY_H
 #define MATRIX_ARRAY_H
+#include <iostream>
 
+typedef unsigned int uint;
 
+template<class T>
 class array {
-
+private:
+    T* start;
+public:
+    array();
+    array(std::initializer_list<T>);
+    ~array();
+    void push_back(T value);
+    void front();
+    T &operator[](int i);
+    uint size();
 };
 
 #include "array.cpp"
