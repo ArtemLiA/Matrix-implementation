a943f00a60ecff4ece7f7f802cdb6976c28c8370
Artem Liakhov <Liakhov.Artem@yandex.ru>
2022-12-01 16:05:48 +0300

    Initial commit

---
+++ array.cpp
@@ -0,0 +1,11 @@
+//
+// Created by lyaho on 01.12.2022.
+//
+
+#ifndef MATRIX_ARRAY_CPP
+#define MATRIX_ARRAY_CPP
+#include "array.h"
+
+
+
+#endif //MATRIX_ARRAY_CPP
---
+++ array.h
@@ -0,0 +1,14 @@
+//
+// Created by lyaho on 01.12.2022.
+//
+
+#ifndef MATRIX_ARRAY_H
+#define MATRIX_ARRAY_H
+
+
+class array {
+
+};
+
+#include "array.cpp"
+#endif //MATRIX_ARRAY_H
